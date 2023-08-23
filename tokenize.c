f604a401185e8517e892e41090e9b9cba4bc4765
natholiR <nathiabera@gmail.com>
2023-08-23 13:07:37 -0700

    update tokenize task

--- tokenize.c
+++ tokenize.c
@@ -7,37 +7,23 @@
 */
 char **tokenize(char *lineptr)
 {
-char **new_array = NULL;     
+char **new_array = NULL;
 char *token_generator = NULL;
-size_t i = 0;                
-int new_length = 0;          
-/**
- * check if lineptr is NULL
- */
+size_t i = 0;
+int new_length = 0;
 if (lineptr == NULL)
-return NULL;
+return (NULL);
 
-/**
- * count number of tokens in lineptr
- *
- */
 for (i = 0; lineptr[i] != '\0'; i++)
 {
 if (lineptr[i] == ' ')
 new_length++;
 }
 
-/**
- * allocate memory for new_array
- *
- */
 new_array = malloc(sizeof(char *) * (new_length + 2));
 if (new_array == NULL)
-return NULL;
+return (NULL);
 
-/**
- * tokenize lineptr and store tokens in new_array
- */
 token_generator = strtok(lineptr, " \n\t\r");
 for (i = 0; token_generator != NULL; i++)
 {
@@ -45,21 +31,13 @@ size_t token_length = strlen(token_generator);
 new_array[i] = malloc(sizeof(char) * (token_length + 1));
 if (new_array[i] == NULL)
 {
-/**
- *  free previously allocated memory
- */
-for (size_t j = 0; j < i; j++)
-free(new_array[j]);
 free(new_array);
-return NULL;
+return (NULL);
 }
 strcpy(new_array[i], token_generator);
 token_generator = strtok(NULL, " \n\t\r");
 }
-new_array[i] = NULL; 
-/*
- *  set last element of new_array to NULL
- */
+new_array[i] = NULL;
 
-return new_array;
+return (new_array);
 }
