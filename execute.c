bddb32528a895ae6fa7bef1b2a57652f02add8c7
natholiR <nathiabera@gmail.com>
2023-08-23 12:53:37 -0700

    update execute task

--- execute.c
+++ execute.c
@@ -7,73 +7,69 @@ char *builtin_str[] = {"exit"};
 int (*builtin_func[])(char **) = {&sh_exit};
 
 /**
- * sh_num_builtins - return the number of builtins.
- * Return: Number of builtins.
- */
+* sh_num_builtins - return the number of builtins.
+* Return: Number of builtins.
+*/
 int sh_num_builtins(void)
 {
-	static int num_builtins = sizeof(builtin_str) / sizeof(char *);
-
-	return (num_builtins);
+static int num_builtins = sizeof(builtin_str) / sizeof(char *);
+return (num_builtins);
 }
 
 /**
- * sh_exit - exit the shell.
- * @args: List of arguments.
- * Return: 0 on success, 1 on failure.
- */
+* sh_exit - exit the shell.
+* @args: List of arguments.
+* Return: 0 on success, 1 on failure.
+*/
 int sh_exit(char **args)
 {
-	return (200);
+return (200);
 }
 
 /**
- * _fork_fun - Creates a child process to execute a command.
- * @arg: List of arguments.
- * @av: List of arguments passed to the program.
- * @env: List of environment variables.
- * @lineptr: Pointer to the line buffer.
- * @np: Number of the current process.
- * @c: Flag to check if the command is in the PATH.
- * Return: 0 on success, 1 on failure.
- */
+* _fork_fun - Creates a child process to execute a command.
+* @arg: List of arguments.
+* @av: List of arguments passed to the program.
+* @env: List of environment variables.
+* @lineptr: Pointer to the line buffer.
+* @np: Number of the current process.
+* @c: Flag to check if the command is in the PATH.
+* Return: 0 on success, 1 on failure.
+*/
 int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c)
 {
-	pid_t child;
-	int stat, i;
-	char *format = "%s: %d: %s: not found\n";
+pid_t child;
+int stat, i;
+char *format = "%s: %d: %s: not found\n";
 
-	if (arg[0] == NULL)
-		return (1);
+if (arg[0] == NULL)
+return (1);
 
-	int num_builtins = sh_num_builtins();
+int num_builtins = sh_num_builtins();
+for (i = 0; i < num_builtins; i++)
+{
+if (strcmp(arg[0], builtin_str[i]) == 0)
+return (builtin_func[i](arg));
+}
 
-	for (i = 0; i < num_builtins; i++)
-	{
-		if (strcmp(arg[0], builtin_str[i]) == 0)
-			return (builtin_func[i](arg));
-	}
+child = fork();
+if (child == 0)
+{
+if (execve(arg[0], arg, env) == -1)
+{
+fprintf(stderr, format, av[0], np, arg[0]);
+if (!c)
+free(arg[0]);
+free(arg);
+free(lineptr);
+exit(errno);
+}
+}
+else
+{
+wait(&stat);
+return (stat);
+}
 
-	child = fork();
-	if (child == 0)
-	{
-		if (execve(arg[0], arg, env) == -1)
-		{
-			fprintf(stderr, format, av[0], np, arg[0]);
-			if (!c)
-				free(arg[0]);
-			free(arg);
-
-			free(lineptr);
-			exit(errno)
-		}
-	}
-
-	else
-	{
-		wait(&stat);
-		return (stat);
-	}
-
-	return (0);
+return (0);
 }
--- value_path.c
+++ value_path.c
@@ -4,90 +4,45 @@
 * _values_path - Concatenates the command with the path.
 * @arg: Argument.
 * @env: Environment.
-* Return: 0 if success, -1 otherwise.
+* Return: 0 if success.
 */
 int _values_path(char **arg, char **env)
 {
 char *new_token = NULL, *relative_path = NULL, *absolute_path = NULL;
 size_t value_path, length;
 struct stat lineptr_status;
-
-/**
-* Check if the argument exists
-*/
 if (stat(*arg, &lineptr_status) == 0)
 return (-1);
-
-/**
-* Get the relative path
-*/
-relative_path = _get_path(env);
+relative_path = _get_path(env); /**gets the path*/
 if (!relative_path)
 return (-1);
-
-/**
-* Tokenize the path
-*/
-new_token = strtok(relative_path, ":");
-
-/**
-* Get the length of the argument
-*/
-length = strlen(*arg);
-
-/**
-* Loop through each tokenized path
-*/
+new_token = strtok(relative_path, ":"); /**tokenizes the path*/
+length = strlen(*arg);                 /**gets the length of the argument*/
 while (new_token)
 {
 value_path = strlen(new_token);
-
-/**
-* Allocate memory for the absolute path
-*/
 absolute_path = malloc(sizeof(char) * (value_path + length + 2));
 if (!absolute_path)
 {
 free(relative_path);
 return (-1);
 }
-
-/**
-* Copy the tokenized path and concatenate the argument
-*/
 absolute_path = strcpy(absolute_path, new_token);
 strcat(absolute_path, "/");
 strcat(absolute_path, *arg);
 
-/**
-*
-* Check if the absolute path exists
-*/
 if (stat(absolute_path, &lineptr_status) == 0)
 {
 *arg = absolute_path;
 free(relative_path);
 return (0);
 }
-
-/**
-* Free the absolute path memory
-*/
 free(absolute_path);
-
-/**
-* Get the next tokenized path
-*/
 new_token = strtok(NULL, ":");
 }
-
-/**
-* Free the relative path memory
-*/
+new_token = '\0';
 free(relative_path);
-
-/**
-* Return -1 if the absolute path does not exist
-*/
+{
 return (-1);
+}
 }
