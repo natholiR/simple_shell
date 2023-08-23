3374a0ffb12a067eac65b4e0dec7caaa41fe7c0a
AHMEDSEID1 <kedi3340@gmail.com>
2023-08-23 11:58:31 +0000

    update main

---
+++ loop.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* _getline_command - it prepare the command line
+*
+* Return: the command line
+*/
+
+char *_getline_command(void)
+{
+char *input = NULL;
+size_t size = 0;
+
+if (isatty(STDIN_FILENO))
+write(STDOUT_FILENO, "$ ", 2);
+
+if (getline(&input, &size, stdin) == -1)
+{
+free(input);
+return (NULL);
+}
+
+return (input);
+}
