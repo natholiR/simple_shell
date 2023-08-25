18fe3eb6551e379ff3b4ee5df553a5f57a7caf4a
AHMEDSEID1 <kedi3340@gmail.com>
2023-08-25 20:17:53 +0000

    update main

--- main.c
+++ main.c
@@ -1,13 +1,56 @@
 #include "main.h"
 
 /**
+ * executeCommand - Execute built-in commands
+ * @command: The command string to execute
+ * @env: Environment variables
+ *
+ * Return: 1 if the command is handled, 0 otherwise
+ */
+int executeCommand(char **command, char **env)
+{
+	if (command == NULL || command[0] == NULL)
+		return (0);
+
+	if (_strcmp(command[0], "cd") == 0)
+	{
+		/* Handle 'cd' command */
+		if (command[1] == NULL)
+		{
+			printf("Invalid usage of 'cd' command.\n");
+		}
+		else
+		{
+			if (chdir(command[1]) == -1)
+			{
+				printf("Directory change failed.\n");
+			}
+		}
+		return (1); /* Indicate that the command was handled */
+	}
+	else if (_strcmp(command[0], "exit") == 0)
+	{
+		/* Handle 'exit' command */
+		printf("Exiting the shell.\n");
+		exit(0);
+	}
+	else if (_strcmp(command[0], "env") == 0)
+	{
+		/* Handle 'env' command */
+		_getenv(env);
+		return (1); /* Indicate that the command was handled */
+	}
+
+	return (0); /* Indicate that the command was not handled */
+}
+
+/**
  * main - Entry point
  * @ac: Count of arguments
  * @av: Arguments
  * @env: Environment variables
  * Return: 0 on success
  */
-
 int main(int ac, char **av, char **env)
 {
 	int pathval = 0, stat = 0, is_path = 0;
@@ -24,20 +67,21 @@ int main(int ac, char **av, char **env)
 			pathval++, commands_array = tokenize(input);
 			if (!commands_array)
 				free(input);
-			if (!_strcmp(commands_array[0], "env"))
-				_getenv(env);
-			else
+			if (executeCommand(commands_array, env))
 			{
-				is_path = _values_path(&commands_array[0], env);
-				stat = _fork_fun(commands_array, av, env, input, pathval, is_path);
-				if (stat == 200)
-				{
-					free(input);
-					return (0);
-				}
-				if (is_path == 0)
-					free(commands_array[0]);
+				free(commands_array);
+				continue;
 			}
+			is_path = _values_path(&commands_array[0], env);
+			stat = _fork_fun(commands_array, av, env, input, pathval, is_path);
+			if (stat == 200)
+			{
+				free(input);
+				return (0);
+			}
+			if (is_path == 0)
+				free(commands_array[0]);
+
 			free(commands_array); /** frees commands_array*/
 		}
 		else
@@ -50,5 +94,3 @@ int main(int ac, char **av, char **env)
 	}
 	return (0);
 }
-
-
